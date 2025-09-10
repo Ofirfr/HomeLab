a49eb72b35decd5175c2f43d12877f967a25089b
Ofirfr <ofir.frisch1@gmail.com>
2025-09-10 20:02:05 +0300

    fix pihole

--- pihole/docker_run.sh
+++ pihole/docker_run.sh
@@ -9,7 +9,7 @@ PIHOLE_BASE="${PIHOLE_BASE:-$(pwd)}"
 docker run -d \
     --name pihole \
     -p 53:53/tcp -p 53:53/udp \
-    -p 8080:80 \
+    -p 8888:80 \
     -e TZ="Asia/Jerusalem" \
     -v "${PIHOLE_BASE}/etc-pihole:/etc/pihole" \
     -v "${PIHOLE_BASE}/etc-dnsmasq.d:/etc/dnsmasq.d" \
